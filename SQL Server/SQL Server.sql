e4018eb27b184843aab82412a4ac90c0d5195018
Ray Kang <adamck6302438@gmail.com>
2019-11-30 01:06:04 -0500

    Update SQL Server.sql
    modified to sql server

--- SQL Server/SQL Server.sql
+++ SQL Server/SQL Server.sql
@@ -4,23 +4,21 @@ USE ProductsDB;
 
 -- 2. Create Product table
 CREATE TABLE Product(
-id INT NOT NULL AUTO_INCREMENT,
+id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
 -- changed column name to product_name to avoid possible confusion from keyword 'name'
-product_name varchar(200) not null, 
-price double not null,
-description varchar(200),
-primary key(id)
+product_name VARCHAR(255) NOT NULL, 
+price FLOAT,
+product_description VARCHAR(255),
 );
 
 -- 3. Insert product data to Product table
-INSERT INTO Product(product_name, price, description)
+INSERT INTO Product(product_name, price, product_description)
 VALUES ('Dog Shampoo', 10.57, 'shampoo for long hair dogs'), 
 	('WD Red 4 TB', 150.99, 'NAS hard drive'), 
 	('2018 Nissan Mourano', 37866.99, 'Nissan Mourano AWD with Tech Package?');
 
--- 4. Create a copy of Product table called 'Product_Copy'
-CREATE TABLE Product_Copy
-SELECT * FROM Product;
+-- 4. Create a copy of Product table called 'Product_Copy' 
+SELECT * INTO Product_Copy FROM Product;
 
 -- 5. Delete 2nd row of Product_Copy table
 DELETE FROM Product_Copy WHERE id = 2;
