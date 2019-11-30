e14be206727fcec58211194bd7d23508e3826a99
Ray Kang <adamck6302438@gmail.com>
2019-11-29 20:53:24 -0500

    Finished step 2 add product table

--- SQL Server/SQL Server.sql
+++ SQL Server/SQL Server.sql
@@ -1,1 +1,12 @@
+-- 1. Create a database, and name it `ProductsDB`.
 CREATE DATABASE ProductsDB;
+
+-- 2. Create Product table
+CREATE TABLE Product(
+id INT NOT NULL AUTO_INCREMENT,
+-- changed column name to product_name to avoid possible confusion from keyword 'name'
+product_name varchar(25) not null, 
+Price double not null,
+description varchar(25),
+primary key(id)
+);
