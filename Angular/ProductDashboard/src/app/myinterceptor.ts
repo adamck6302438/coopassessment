5c16a667e0915264e82772ae01a8acac854dbb23
Ray Kang <adamck6302438@gmail.com>
2019-11-30 20:55:57 -0500

    Create myinterceptor.ts
    added interceptor

---
+++ Angular/ProductDashboard/src/app/myinterceptor.ts
@@ -0,0 +1,29 @@
+import { Injectable } from "@angular/core";
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpResponse,
+  HttpErrorResponse,
+  HttpHeaders
+} from "@angular/common/http";
+import { Observable } from "rxjs/Observable";
+import { nextTick } from 'q';
+
+@Injectable()
+export class MyInterceptor implements HttpInterceptor {
+    constructor() { }
+    intercept(
+        request: HttpRequest<any>,
+        next: HttpHandler
+      ): Observable<HttpEvent<any>> {
+        //how to update the request Parameters
+        const updatedRequest = request.clone({
+          headers: new HttpHeaders().append('httpInterceptor', 'DummyInterceptor')
+        });
+
+        return next.handle(updatedRequest);
+    }
+    
+}
